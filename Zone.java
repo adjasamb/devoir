5c7792f327cb3a7b57cff5563530e8f3e1c27a87
adjasamb <159190463+adjasamb@users.noreply.github.com>
2024-02-19 21:08:06 +0000

    Add files via upload

---
+++ Zone.java
@@ -0,0 +1,28 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class zone {
+    private int id;
+    private String nom; 
+}
+public List<zone> getzone() {
+        return zone;
+    }
+
+    public void setzone(List<zone> zone) {
+        this.comptes = comptes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getnom() {
+        return nom;
+    }
